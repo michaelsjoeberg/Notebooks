c8fcc115273b3cc715aa720273e1a0c2c760b619
Michael Sjoeberg <mail@michaelsjoeberg.com>
2016-10-08 19:43:38 +0200

    init

---
+++ java/other/HelloWorld.java
@@ -0,0 +1,14 @@
+/*
+*	Run in Terminal
+*	1. javac HelloWorld.java
+*	2. java HelloWorld
+*/
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// print 'Hello World'
+		System.out.println("Hello World");
+	}
+
+}
