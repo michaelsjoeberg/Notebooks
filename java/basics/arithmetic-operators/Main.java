74ee601625bc69821ea4470a264433fedf454f13
Michael Sjoeberg <mail@michaelsjoeberg.com>
2016-10-09 16:59:29 +0200

    arithmetic operators java

---
+++ java/basics/arithmetic-operators/Main.java
@@ -0,0 +1,44 @@
+/******************************************
+ *  Compilation:  javac Main.java
+ *  Execution:    java Main
+ *
+ *  Prints the result from arithmetic 
+ *	operations.
+ *
+ ******************************************/
+
+import java.lang.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// example variables
+		double a = 10.0;
+		double b = 20.0;
+
+		// addition
+		System.out.println(a + b); 					// a + b
+		System.out.println(a + (a + b)); 			// a + (a + b)
+
+		// subtraction
+		System.out.println(b - a); 					// b - a
+		System.out.println(b - b); 					// b - b
+
+		// multiplication
+		System.out.println(a * b); 					// a * b
+		System.out.println(a * (a * 2)); 			// a * (a * 2)
+
+		// division
+		System.out.println(b / a);					// b / a
+		System.out.println((a * b) / a); 			// (a * b) / a
+
+		// modulus
+		System.out.println(b % a); 					// b % a
+		System.out.println(a % b);					// a % b
+
+		// exponent
+		System.out.println(Math.pow(a, 2));			// a^2
+		System.out.println(Math.pow(b, (a / 10)));	// b^(a / 10)
+	}
+
+}
