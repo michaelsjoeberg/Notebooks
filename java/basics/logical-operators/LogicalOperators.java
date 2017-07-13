958a0c91ebade10809e5a8d671c950342d9a899e
Michael Sjoeberg <mail@michaelsjoeberg.com>
2017-07-13 16:34:03 +0200

    java update
    logical operators

--- java/basics/logical-operators/LogicalOperators.java
+++ java/basics/logical-operators/LogicalOperators.java
@@ -1,48 +1,91 @@
-/******************************************
- *  Compilation:  javac LogicalOperators.java
- *  Execution:    java LogicalOperators
+/******************************************************
  *
- *  Prints the result from logical 
- *	operations.
+ *  Compute logical operations.
  *
- ******************************************/
+ *  Author 			: Michael Sjoeberg (github.com/mqe)
+ *
+ *  Date 			: 13 July 2017
+ *  Updated 		: 13 July 2017 (Michael Sjoeberg)
+ * 
+ *  Compilation 	: javac LogicalOperators.java
+ *  Execution 		: java LogicalOperators
+ *
+ ******************************************************/
 
 public class LogicalOperators {
 
 	public static void main(String[] args) {
+
+		// print credits
+		System.out.println(
+			"---------------------------------------------------\n" +
+			"Program   : Logical Operators\n" +
+			"Author    : Michael Sjoeberg (github.com/mqe)\n" +
+			"Date      : 13 July 2017\n" +
+			"Updated   : 13 July 2017 (Michael Sjoeberg)\n" +
+			"---------------------------------------------------\n");
+		
 		// example variables
 		int A = 3;
 		int B = 5;
 		boolean T = true;
 		boolean F = false;
 
-		// print 'equal to'
-		System.out.println(A == B); 			// false
+		// print example variables
+		System.out.println("A = " + A);
+		System.out.println("B = " + B);
+		System.out.println("T = " + T);
+		System.out.println("F = " + F);
+		System.out.println(" ");
 
-		// print 'not equal to'
-		System.out.println(A != B);				// true
+		/**
+		 * Equal
+		 */
+		System.out.println("Equal:");
+		System.out.println("(A == B) -> " + (A == B));
+		System.out.println("(A == A) -> " + (A == A));
+		System.out.println(" ");
 
-		// print 'greater than'
-		System.out.println(A > B); 				// false
+		/**
+		 * Not equal
+		 */
+		System.out.println("Not equal:");
+		System.out.println("(A != B) -> " + (A != B));
+		System.out.println("(A != A) -> " + (A != A));
+		System.out.println(" ");
 
-		// print 'less than'
-		System.out.println(A < B); 				// true
+		/**
+		 * Greater than
+		 */
+		System.out.println("Greater than:");
+		System.out.println("(A > B) -> " + (A > B));
+		System.out.println("(B > A) -> " + (B > A));
+		System.out.println(" ");
 
-		// print 'greater than or equal to'
-		System.out.println(A >= B); 			// false
+		/**
+		 * Less than
+		 */
+		System.out.println("Less than:");
+		System.out.println("(A < B) -> " + (A < B));
+		System.out.println("(B < A) -> " + (B < A));
+		System.out.println(" "); 			
 
-		// print 'less than or equal to'
-		System.out.println(A <= B); 			// true
+		/**
+		 * Greater/ less than or equal
+		 */
+		System.out.println("Greater/ less than or equal:");
+		System.out.println("(A >= B) -> " + (A >= B));
+		System.out.println("(A <= A) -> " + (A <= A));
+		System.out.println(" ");		
 
-		// print 'and'
-		System.out.println(T && F); 			// false
-
-		// print 'or'
-		System.out.println(T || F); 			// true
-
-		// print 'not'
-		System.out.println(!T);					// false
-
+		/**
+		 * And, or, not
+		 */
+		System.out.println("And, or, not");
+		System.out.println("(T && F) -> " + (T && F));
+		System.out.println("(T || F) -> " + (T || F));
+		System.out.println("!T -> " + !T);
+		System.out.println(" ");						
 	}
 
 }
